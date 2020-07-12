0f70f2bbbe1e3f706d9d3eb3637046925af37753
Sips <batlefield.desert.combat@gmail.com>
2020-07-12 23:10:23 +0300

    [VVPS] Added .gitignore and removed .vs/ and out/ folders from Version Control

--- out/build/x64-Debug (default)/lib/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: H:/FrequentPatternVVPS_new/FrequentPatternVVPS/lib/googletest
-# Build directory: H:/FrequentPatternVVPS_new/FrequentPatternVVPS/out/build/x64-Debug (default)/lib/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- out/build/x64-Debug (default)/lib/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: H:/FrequentPatternVVPS_new/FrequentPatternVVPS/lib/googletest/googletest
-# Build directory: H:/FrequentPatternVVPS_new/FrequentPatternVVPS/out/build/x64-Debug (default)/lib/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
